c25dfb65b4143e650a75562b1528768ec0db829a
Umme Salma <ummes3527@gmail.com>
2021-10-27 12:48:16 -0700

    done

--- index.js
+++ index.js
@@ -4,7 +4,20 @@ const jokes = document.querySelector("#joke");
 const jokeBtn = document.querySelector("#jokeBtn");
 
 // now defining the function for generating jokes
-const generateJokes = () => {};
+const generateJokes = async () => {
+  try {
+    const setHeader = {
+      headers: {
+        Accept: "application/json",
+      },
+    };
+    const res = await fetch("https://icanhazdadjoke.com/", setHeader);
+    const data = await res.json();
+    data.innerHTML = data.joke;
+  } catch (err) {
+    console.log(`the errpr is ${err}`);
+  }
+};
 
 jokeBtn.addEventListener("click", generateJokes);
 generateJokes();
