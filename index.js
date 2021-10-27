ee39d8081660a479f6e62c04941fd269684910a5
Umme Salma <ummes3527@gmail.com>
2021-10-27 11:41:53 -0700

    initial-commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// get https://icanhazdadjoke.com/
+
+const jokes = document.querySelector("#joke");
+const jokeBtn = document.querySelector("#jokeBtn");
+
+// now defining the function for generating jokes
+const generateJokes = () => {};
+
+jokeBtn.addEventListener("click", generateJokes);
+generateJokes();
